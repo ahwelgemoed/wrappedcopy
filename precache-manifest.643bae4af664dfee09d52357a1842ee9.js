1800b10e99f53b29098dea033ef06098bfb519f9
ahwelgemoed <arno.welgemoed@outlook.com>
2020-07-18 19:35:55 +0200

    Updates

---
+++ precache-manifest.643bae4af664dfee09d52357a1842ee9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "731b7c2f05267200ac404480f4f3430e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "efb26675b59b018afcce",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "c6e0c19a4de94fe6c7e6",
+    "url": "./static/js/2.5880372a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.5880372a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efb26675b59b018afcce",
+    "url": "./static/js/main.61adcaeb.chunk.js"
+  },
+  {
+    "revision": "272bbbe592b470fdd734",
+    "url": "./static/js/runtime-main.0161b2af.js"
+  }
+]);
